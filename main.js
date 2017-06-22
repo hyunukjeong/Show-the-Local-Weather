57b0bd16ccf85de0cc20054610d57787fd20336a
HyunUk Jeong <hjeong326@gmail.com>
2017-06-22 17:17:05 -0500

    Added Geolocation

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
 $("document").ready(function() {
 
 	$side = $(".col-xs-3");
+	$geolocation = $("#geolocation");
+	$locationButton = $("#location-button");
 
 
 	$side.html("<br><br><br><br>").css("background-color", "blue");
 
+	$locationButton.on("click", function() {
+		getGeolocation();
+	});
+
 
 });
+
+// Success callback for getGeolocation()
+function geo_success(position) {
+	$geolocation.html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+}
+
+// Error callback for getGeolocation()
+function geo_error() {
+	alert("No position available");
+}
+
+// Callback for Get Geolocation Button
+function getGeolocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(geo_success, geo_error);
+	}
+}
+
+// Initial getGeolocation() method
+// function getGeolocation() {	
+// 	if (navigator.geolocation) {
+// 		navigator.geolocation.getCurrentPosition(function(position) {
+// 			$geolocation.html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+// 		});
+// 	} else {
+// 		console.log("geolocation object does not exist");
+// 	}
+// }
+
+
