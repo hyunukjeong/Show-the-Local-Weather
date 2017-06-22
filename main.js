33fcbb30d31fc3dee6f8615d465d0ee23ed965e3
HyunUk Jeong <hjeong326@gmail.com>
2017-06-22 16:24:16 -0500

    initial setup

---
+++ main.js
@@ -0,0 +1,9 @@
+$("document").ready(function() {
+
+	$side = $(".col-xs-3");
+
+
+	$side.html("<br><br><br><br>").css("background-color", "blue");
+
+
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Show the Local Weather #
+
+```
+Uses HTML5 Geolocation to show the local weather 
+```
